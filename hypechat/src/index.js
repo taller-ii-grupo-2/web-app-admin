cb3b11fa89398df66610d56671bed4b5d0dc8e65
augustoarturi <turi.77@hotmail.com>
2019-04-01 12:22:01 -0300

    starting okta probe

--- hypechat/src/index.js
+++ hypechat/src/index.js
@@ -3,8 +3,19 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+//From OKta
+import { Router } from 'react-router-dom'
+import { createBrowserHistory } from 'history'
+import Main from './Main';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const history = createBrowserHistory();
+
+
+ReactDOM.render((
+  <Router history={history}>
+    <Main history={history} />
+  </Router>
+), document.getElementById('root'))
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
